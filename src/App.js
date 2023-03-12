7a9e965dcded9ae968a65ac57ffaabc422d7b2c8
Ruslan1344 <vadimrostovskiy146@gmail.com>
2023-03-12 15:07:52 +0200

    add

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Contact from "./routes/Contact";
+import Home from "./routes/Home";
+import Pricing from "./routes/Pricing";
+import Training from "./routes/Training";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/training" element={<Training />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
